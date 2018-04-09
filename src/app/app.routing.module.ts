2e48c933c9336afe3a14fa9395f91971aaf22a1c
Anderson Ribeiro <andersonribeiro.si@hotmail.com>
2018-04-09 16:55:07 -0300

    Resolvido alguns bugs

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from "@angular/router";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { NgModule } from "@angular/core";
 import { AddItemComponent } from "./componentes/add-item/add-item.component";
-import { CadUsuarioComponent } from "./componentes/cad-usuario/cad-usuario.component";
 import { LoginappComponent } from "./loginapp/loginapp.component";
 import { AuthGuard } from "./guards/auth.guard";
 
@@ -20,7 +19,6 @@ const APP_ROUTES: Routes = [
     path: 'add-item', component: AddItemComponent,
     canActivate: [AuthGuard]
   },
-  { path: 'cad-usuario', component: CadUsuarioComponent },
   {
     path: 'login-app', component: LoginappComponent,
 
